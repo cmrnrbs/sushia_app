6a857171103162f058bdc4b551a66ddd5b6aac81
Cemre Onur Baş <cemreonurb@gmail.com>
2020-10-09 15:36:25 +0300

    Initial Commit

---
+++ sushia_app/sushia_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  sushia_appApp.swift
+//  sushia_app
+//
+//  Created by Cemre Onur Baş on 9.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct sushia_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ sushia_appTests/sushia_appTests.swift
@@ -0,0 +1,33 @@
+//
+//  sushia_appTests.swift
+//  sushia_appTests
+//
+//  Created by Cemre Onur Baş on 9.10.2020.
+//
+
+import XCTest
+@testable import sushia_app
+
+class sushia_appTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
